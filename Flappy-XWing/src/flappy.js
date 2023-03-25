88d25534f6d1e399c246e73023c770b77727dc26
Resistance205 <76964444+Resistance205@users.noreply.github.com>
2023-03-25 21:52:54 +0000

    CSS and Canvas
    Added and updated the CSS file, and also added a canvas to display basic geometric shapes

--- Flappy-XWing/src/flappy.js
+++ Flappy-XWing/src/flappy.js
@@ -1,10 +1,31 @@
-window.onload = function() {
+window.onload = function() 
+{
+    
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext("2d");
+
+    //Left to right wing
+    context.moveTo(50, 100);
+    context.lineTo(400,200);
+    context.stroke();
+
+
+    //Right to left wing 
+    context.moveTo(400, 50);
+    context.lineTo(50,100);
+    context.stroke();
+
+    // alert("Please don't sue me, Disney...");
     let btn = document.getElementById('jump');
     let count = 0;
 
-    btn.onclick = function() {
+    btn.onclick = function() 
+    {
         count++;
         console.log("Count = " + count);
     }
+
+
+
 
 }
